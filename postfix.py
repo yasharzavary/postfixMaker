1f67d2934ddb480378ec352fe0113b5075e1f4e1
yashar <zavaryyashar009@gmail.com>
2023-12-16 17:19:54 +0330

    files add and ready for writing

---
+++ postfix.py
@@ -0,0 +1,13 @@
+# creted by: yashar zavary rezaie
+
+"""_summary_
+
+make one class that change one infix to postfix form,
+here we have two type:
+first: it can have numeric type, we will postfix it and solve it in this class
+second: variable type, we will convert it and make one chart from that form
+
+"""
+
+class postFix:
+    pass
