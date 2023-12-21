b1fea676309e14c502d6131511f8465aeaa83286
yashar <zavaryyashar009@gmail.com>
2023-12-21 14:51:01 +0330

    class named and ready to start

---
+++ postClass.py
@@ -0,0 +1,18 @@
+"""
+created by: yashar zavary rezaie
+
+"""
+
+
+class post:
+    def __init__(self):
+        pass
+
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+    def __del__(self):
+        pass
